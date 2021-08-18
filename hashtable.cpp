452d511e8dd00e7bf90d0e0e18dc7b293ec0b38e
Thomas Dokas <dokastho@umich.edu>
2021-08-18 10:32:53 -0400

    modified:   hashtable.cpp 	modified:   hashtable.h

---
+++ hashtable.cpp
@@ -0,0 +1,15 @@
+#include "hashtable.h"
+
+/*                      HASH TABLE SNAPSHOT
+*   The purpose of this file is to demonstrate my understanding
+*   of the c++ STL unordered_map. This will include the following 
+*   collision resolution methods: seperate chaining, quadratic 
+*   probing and double hashing. These three methods are 
+*   fundementally different in concept, and are therefore included
+*   in this snapshot. Not included is linear probing due to it’s 
+*   similarity and inferiority to quadratic probing
+*/
+
+int main() {
+    return 0;
+}
---
+++ hashtable.h
@@ -0,0 +1,13 @@
+#ifndef HASH_H
+#define HASH_H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class hashtable {
+
+};
+
+#endif
